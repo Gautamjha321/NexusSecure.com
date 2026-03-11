68b8f61fa7327ae7b896db1d4fdc3e88d3a28bb6
Gautamjha321 <jhagautam168@gmail.com>
2026-03-11 14:26:03 +0530

    updates

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -96,15 +96,14 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', 'postgres'),
-        'USER': os.getenv('DB_USER', 'postgres'),
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
-
 
 # ================================
 # PASSWORD VALIDATION
